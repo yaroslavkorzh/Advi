54f2f543c8cb2b01f561ac8f984cae7d90ad80a9
Arthur Dombrovski <pandombrovski@gmail.com>
2015-10-17 19:58:29 -0400

    Feature(app.js, config.js): added configuration file

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    database: {
+        host     : 'localhost',
+        user     : 'root',
+        password : '123456aA',
+        database : 'test',
+        port: 3306
+    }
+};
+
+module.exports = config;
